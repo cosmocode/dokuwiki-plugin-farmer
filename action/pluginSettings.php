e8b4b17016e5c4c26cb349952d5a2cd22d76bf58
Michael Große <grosse@cosmocode.de>
2015-08-19 17:53:08 +0200

    Add ajax functionality to singleAnimalEdit

---
+++ action/pluginSettings.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ *
+ * @author     Michael Große <grosse@cosmocode.de>
+ */
+
+if(!defined('DOKU_INC')) die();
+
+/**
+ * Class action_plugin_farmer_pluginSettings
+ */
+class action_plugin_farmer_pluginSettings extends DokuWiki_Action_Plugin {
+
+    /**
+     * plugin should use this method to register its handlers with the DokuWiki's event controller
+     *
+     * @param Doku_Event_Handler $controller DokuWiki's event controller object. Also available as global $EVENT_HANDLER
+     *
+     */
+    function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this,'_ajax_call');
+    }
+
+    /**
+     * handle ajax requests
+     *
+     * @param Doku_Event $event
+     * @param $param
+     */
+    function _ajax_call(Doku_Event $event, $param) {
+        if (substr($event->data, 0, 13) !== 'plugin_farmer') {
+            return;
+        }
+        //no other ajax call handlers needed
+        $event->stopPropagation();
+        $event->preventDefault();
+
+        $animal = substr($event->data, 14);
+
+        $plugins = array();
+        //include(DOKU_FARMDIR . $animal . '/conf/plugins.local.php');
+        include(DOKU_INC . '/conf/plugins.local.php');
+        $data = $plugins;
+
+        //json library of DokuWiki
+        require_once DOKU_INC . 'inc/JSON.php';
+        $json = new JSON();
+
+        //set content type
+        header('Content-Type: application/json');
+        echo $json->encode($data);
+    }
+
+}
+
--- scripts/plugins.js
+++ scripts/plugins.js
@@ -10,8 +10,44 @@
     };
 
     jQuery(document).ready(function () {
-        jQuery('#farmer__animalSelect').chosen({
+        var bulkPluginSelector = jQuery('#farmer__bulkPluginSelect');
+        bulkPluginSelector.chosen({
             search_contains: true
+        });
+        bulkPluginSelector.change(function () {
+            jQuery(".bulkButton").prop('disabled',false);
+        });
+
+        var animalSelector = jQuery('#farmer__animalSelect');
+        animalSelector.chosen({
+            search_contains: true
+        });
+        animalSelector.change(function () {
+            console.log('change event received');
+            var animal = animalSelector.val();
+            jQuery.post(
+                DOKU_BASE + 'lib/exe/ajax.php',
+                {
+                    call: 'plugin_farmer_' + animal
+                },
+                function(data) {
+                    alert('Received response ' + JSON.stringify(data,null,2));
+                    // data is array you returned with action.php
+                },
+                'json'
+            );
+        });
+        jQuery("input[name=bulkSingleSwitch]:radio").change(function () {
+            if (jQuery('#farmer__bulk').prop("checked")) {
+                jQuery('#farmer__bulkForm').css('display','initial');
+            } else {
+                jQuery('#farmer__bulkForm').css('display','none');
+            }
+            if (jQuery('#farmer__single').prop("checked")) {
+                jQuery('#farmer__singlePluginForm').css('display','initial');
+            } else {
+                jQuery('#farmer__singlePluginForm').css('display','none');
+            }
         });
     });
 
