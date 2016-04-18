d3a4cfaa2244808d7d55da8adca89827b825b91b
Andreas Gohr <gohr@cosmocode.de>
2016-04-18 17:30:53 +0200

    use dropdown type for plugins

--- admin/plugins.php
+++ admin/plugins.php
@@ -68,47 +68,34 @@ class admin_plugin_farmer_plugins extends DokuWiki_Admin_Plugin {
         $switchForm = new \dokuwiki\Form\Form();
         $switchForm->addClass('plugin_farmer');
         $switchForm->addFieldsetOpen($this->getLang('bulkSingleSwitcher'));
-        $switchForm->addRadioButton('bulkSingleSwitch', $this->getLang('bulkEdit'))->id('farmer__bulk')->attr('type','radio')->addClass('block');
-        $switchForm->addRadioButton('bulkSingleSwitch', $this->getLang('singleEdit'))->id('farmer__single')->attr('type','radio')->addClass('block');
+        $switchForm->addRadioButton('bulkSingleSwitch', $this->getLang('bulkEdit'))->id('farmer__bulk')->attr('type','radio');
+        $switchForm->addRadioButton('bulkSingleSwitch', $this->getLang('singleEdit'))->id('farmer__single')->attr('type','radio');
         $switchForm->addFieldsetClose();
         echo $switchForm->toHTML();
 
         /** @var helper_plugin_farmer $helper */
         $helper = plugin_load('helper', 'farmer');
         $plugins = $helper->getAllPlugins();
+        array_unshift($plugins, '');
 
         $bulkForm = new \dokuwiki\Form\Form();
         $bulkForm->id('farmer__bulkForm');
         $bulkForm->addClass('plugin_farmer');
         $bulkForm->addFieldsetOpen($this->getLang('bulkEditForm'));
-        $bulkForm->addTagOpen('select')->id('farmer__bulkPluginSelect')->attr('name','farmer__bulkPluginSelect');
-        $bulkForm->addTagOpen('option')->attr('selected', 'selected')->attr('disabled', 'disabled')->attr('hidden', 'hidden')->attr('value', "");
-        $bulkForm->addTagClose('option');
-        foreach ($plugins as $plugin) {
-            $bulkForm->addTagOpen('option')->attr('value', $plugin);
-            $bulkForm->addHTML($plugin);
-            $bulkForm->addTagClose('option');
-        }
-        $bulkForm->addTagClose('select');
-        $bulkForm->addButton('farmer__submitBulk',$this->getLang('activate'))->attr('value','activate')->attr('type','submit')->attr('disabled','disabled')->addClass('bulkButton');
-        $bulkForm->addButton('farmer__submitBulk',$this->getLang('deactivate'))->attr('value','deactivate')->attr('type','submit')->attr('disabled','disabled')->addClass('bulkButton');
+        $bulkForm->addDropdown('farmer__bulkPluginSelect', $plugins)->id('farmer__bulkPluginSelect');
+        $bulkForm->addButton('farmer__submitBulk',$this->getLang('activate'))->attr('value','activate')->attr('type','submit')->attr('disabled','disabled');
+        $bulkForm->addButton('farmer__submitBulk',$this->getLang('deactivate'))->attr('value','deactivate')->attr('type','submit')->attr('disabled','disabled');
         $bulkForm->addFieldsetClose();
         echo $bulkForm->toHTML();
+
+        $animals = $helper->getAllAnimals();
+        array_unshift($animals, '');
 
         $singleForm = new \dokuwiki\Form\Form();
         $singleForm->id('farmer__singlePluginForm');
         $singleForm->addClass('plugin_farmer');
         $singleForm->addFieldsetOpen($this->getLang('singleEditForm'));
-        $singleForm->addTagOpen('select')->id('farmer__animalSelect')->attr('name', 'plugin_farmer[selectedAnimal]');
-        $singleForm->addTagOpen('option')->attr('selected', 'selected')->attr('disabled', 'disabled')->attr('hidden', 'hidden')->attr('value', "");
-        $singleForm->addTagClose('option');
-        $animals = $helper->getAllAnimals();
-        foreach ($animals as $animal) {
-            $singleForm->addTagOpen('option');
-            $singleForm->addHTML($animal);
-            $singleForm->addTagClose('option');
-        }
-        $singleForm->addTagClose('select');
+        $singleForm->addDropdown('plugin_farmer[selectedAnimal]', $animals)->id('farmer__animalSelect');
         $singleForm->addTagOpen('div')->id('farmer__animalPlugins');
         $singleForm->addTagClose('div');
         $switchForm->addFieldsetClose();
