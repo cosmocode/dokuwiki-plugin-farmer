a1991f43e59ec723bfd58485749d723fd5544e78
Schplurtz le Déboulonné <Schlurtz@laposte.net>
2017-04-04 18:10:23 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,13 +1,9 @@
 <?php
+
 /**
- * French language file for farmer plugin
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
-
-// keys need to match the config setting name
-$lang['deactivated plugins'] = 'Liste à virgule des greffons qui sont désactivés par défaut dans les nouveaux animaux.';
-$lang['disable_new_plugins'] = 'Désactiver automatiquement les greffons après qu\'il sont installés dans le fermier. Valable seulement pour les greffons installés par le gestionnaire d\'extensions.';
-
-
-//Setup VIM: ex: et ts=4 :
+$lang['deactivated plugins']   = 'Liste à virgule des greffons qui sont désactivés par défaut dans les nouveaux animaux.';
+$lang['disable_new_plugins']   = 'Désactiver automatiquement les greffons après qu\'il sont installés dans le fermier. Valable seulement pour les greffons installés par le gestionnaire d\'extensions.';
