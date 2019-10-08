9c4112b1143dd4d110bd542b6b72fc606d1a1109
Bartek S <sadupl@gmail.com>
2019-10-08 10:30:10 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['tab_setup']             = 'Ustawienia Farmy';
+$lang['tab_info']              = 'Informacje';
+$lang['tab_config']            = 'Konfiguracja';
+$lang['tab_plugins']           = 'Zarządzaj pluginami';
+$lang['tab_new']               = 'Dodaj nowe zwierzę';
+$lang['tab_delete']            = 'Usuń zwierzę';
+$lang['preloadPHPForm']        = 'Zainicjuj Farmę';
+$lang['farm dir']              = 'Katalog zwierząt';
+$lang['htaccess setup']        = 'Dodać kod farmy do .htaccess?';
+$lang['submit']                = 'Zatwierdź';
+$lang['farmdir_missing']       = 'Proszę podać katalog gdzie Zwierzęta powinny być przechowywane.';
+$lang['farmdir_in_dokuwiki']   = 'Katalog Zwierząt (%s) musi być poza Farmą dokuwiki (%s).';
+$lang['farmdir_uncreatable']   = 'Katalog Zwierząt (%s) nie został stworzony. Czy uprawnienia są prawidłowe?';
+$lang['farmdir_unwritable']    = 'Proszę się upenić, że webserver posiada prawa zapisu do katalogu Zwierząt.';
+$lang['farmdir_notEmpty']      = 'Katalog Zwierząt (%s) musi być pusty.';
+$lang['preload creation success'] = 'Farma została zainicjowana pomyślnie.';
+$lang['preload creation error'] = 'Wystąpił błąd podczas inicjowania Farmy.';
+$lang['overwrite_preload']     = 'Uwaga: Dotychczasowy plik inc/preload.php będzie zastąpiony podczas kontynuowania!';
+$lang['animal']                = 'Imię Zwierzęta / Domeny';
+$lang['thisis']                = 'Instancja to';
+$lang['thisis.farmer']         = 'Farmer!';
+$lang['thisis.animal']         = 'Zwierzę!';
+$lang['baseinstall']           = 'Instalacja Farmera';
+$lang['animals']               = 'Zwierzęta';
+$lang['confdir']               = 'Katalog konfiguracji instancji';
+$lang['savedir']               = 'Katalog danych instancji';
+$lang['plugins']               = 'Aktywne pluginy tej instancji';
+$lang['base']                  = 'Konfiguracja Bazy';
+$lang['farm host']             = 'Host Name Farmera';
+$lang['save']                  = 'Zapisz';
+$lang['animal template']       = 'Kopiuj istniejące Zwierzę';
+$lang['animal configuration']  = 'Podstawowa konfiguracja Zwierząt';
