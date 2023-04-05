5b5c8486c2fd499fa1aa5be2b0fd2e2fcb622f06
Andreas Gohr <andi@splitbrain.org>
2023-04-05 09:16:44 +0200

    fixed tests

--- _test/core.test.php
+++ _test/core.test.php
@@ -1,10 +1,21 @@
 <?php
-namespace plugin\struct\test;
+
+namespace plugin\farmer\test;
 
 require_once(__DIR__ . '/../DokuWikiFarmCore.php');
 
-class DokuWikiFarmCore extends \DokuWikiFarmCore {
-    public function getAnimalNamesForHost($host) {
+class DokuWikiFarmCore extends \DokuWikiFarmCore
+{
+    /** @noinspection PhpMissingParentConstructorInspection */
+    public function __construct()
+    {
+        $this->loadConfig();
+        // we do not intitialize anyting else here because it's already too late and DOKU_INC is already set
+    }
+
+
+    public function getAnimalNamesForHost($host)
+    {
         return parent::getAnimalNamesForHost($host);
     }
 }
@@ -14,16 +25,18 @@ class DokuWikiFarmCore extends \DokuWikiFarmCore {
  * @group plugin_farmer
  * @group plugins
  */
-class core_plugin_farmer_test extends \DokuWikiTest {
+class core_plugin_farmer_test extends \DokuWikiTest
+{
 
-    protected $pluginsEnabled = array('farmer');
+    protected $pluginsEnabled = ['farmer'];
 
 
-    public function test_hostsAnimals() {
+    public function test_hostsAnimals()
+    {
         $core = new DokuWikiFarmCore();
 
         $input = 'www.foobar.example.com:8000';
-        $expect = array(
+        $expect = [
             'www.foobar.example.com.8000',
             'foobar.example.com.8000',
             'www.foobar.example.com',
@@ -33,7 +46,7 @@ class core_plugin_farmer_test extends \DokuWikiTest {
             'www.foobar',
             'foobar',
             'www',
-        );
+        ];
 
         $this->assertEquals($expect, $core->getAnimalNamesForHost($input));
     }
--- _test/getUserLine.test.php
+++ _test/getUserLine.test.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace plugin\struct\test;
+namespace plugin\farmer\test;
 
-class admin_plugin_farmer_new extends \admin_plugin_farmer_new {
-    public function getAdminLine() {
+class admin_plugin_farmer_new extends \admin_plugin_farmer_new
+{
+    public function getAdminLine()
+    {
         return parent::getAdminLine();
     }
 
@@ -16,19 +18,22 @@ class admin_plugin_farmer_new extends \admin_plugin_farmer_new {
  * @group plugin_farmer
  * @group plugins
  */
-class getUserLine_plugin_farmer_test extends \DokuWikiTest {
+class getUserLine_plugin_farmer_test extends \DokuWikiTest
+{
 
     protected $pluginsEnabled = array('farmer',);
     private $usersfile;
 
-    public function setUp() {
+    public function setUp(): void
+    {
         parent::setUp();
         $this->usersfile = DOKU_CONF . 'users.auth.php';
         copy($this->usersfile, $this->usersfile . "org");
         unlink($this->usersfile);
     }
 
-    public function tearDown() {
+    public function tearDown(): void
+    {
         parent::tearDown();
         unlink($this->usersfile);
         copy($this->usersfile . "org", $this->usersfile);
@@ -36,7 +41,8 @@ class getUserLine_plugin_farmer_test extends \DokuWikiTest {
     }
 
 
-    public function test_getUserLine_oneUser () {
+    public function test_getUserLine_oneUser()
+    {
         $helper = new admin_plugin_farmer_new();
         $usersfileData = "# users.auth.php
 # <?php exit()?>
@@ -50,7 +56,7 @@ class getUserLine_plugin_farmer_test extends \DokuWikiTest {
 #
 # testuser : testpass
 testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:\n";
-        file_put_contents($this->usersfile,$usersfileData);
+        file_put_contents($this->usersfile, $usersfileData);
 
         $_SERVER['REMOTE_USER'] = 'testuser';
         $expected_result = 'testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:' . "\n";
@@ -59,7 +65,8 @@ testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:\n";
         $this->assertSame($expected_result, $actual_result);
     }
 
-    public function test_getUserLine_manyUser () {
+    public function test_getUserLine_manyUser()
+    {
         $helper = new admin_plugin_farmer_new();
         $usersfileData = "# users.auth.php
 # <?php exit()?>
@@ -75,7 +82,7 @@ testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:\n";
 1testuser:179ad45c6ce43897cf1029e212046e81:Arthur Dent:brthur@example.com:admin
 testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:
 2testuser:179ad45c6ce2cb97cf10214712046e81:Arthur inDent:crthur@example.com:admin\n";
-        file_put_contents($this->usersfile,$usersfileData);
+        file_put_contents($this->usersfile, $usersfileData);
 
         $_SERVER['REMOTE_USER'] = 'testuser';
         $expected_result = 'testuser:179ad45c6ce2cb97cf1029e212046e81:Arthur Dent:arthur@example.com:' . "\n";
