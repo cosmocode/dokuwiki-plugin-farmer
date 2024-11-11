107b329db3411678bd8cfaafd4a6b07c4c60e833
Karsten Kosmala <kosmala@cosmocode.de>
2024-11-11 19:48:44 +0100

    add doc blocks

--- remote.php
+++ remote.php
@@ -16,19 +16,44 @@ class remote_plugin_farmer extends RemotePlugin {
         $this->helper = plugin_load('helper', 'farmer');
     }
 
-    public function getHostname() {
+    /**
+     * Get the configured farm host
+     *
+     * @return string
+     */
+    public function getFarmhost(): string
+    {
         return $this->helper->getConfig()['base']['farmhost'];
     }
 
-    public function getBaseDomain() {
+    /**
+     * Get the configured base domain of the farmer
+     * This could be an empty string, then farmhost will be used to determine an animal url
+     *
+     * @return string
+     */
+    public function getBaseDomain(): string
+    {
         return $this->helper->getConfig()['base']['basedomain'];
     }
 
-    public function listAnimals() {
+    /**
+     * Get a list of all animal names
+     *
+     * @return array
+     */
+    public function listAnimals(): array
+    {
         return $this->helper->getAllAnimals();
     }
 
-    public function listAnimalUrls() {
+    /**
+     * Get a list of all animal urls
+     *
+     * @return array
+     */
+    public function listAnimalUrls(): array
+    {
         foreach($this->helper->getAllAnimals() as $animal) {
             $animalUrls[] = $this->helper->getAnimalURL($animal);
         }
